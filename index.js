dc0bb2aa460afd94354d14606886558d9c18c804
Mykhailo Yaremenko <mykhailo.yaremenko@outlook.com>
2024-02-22 19:59:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import http from "node:http";
+
+const port = 8080;
+const hostname = "localhost";
+
+const throwError = (res, code) => {
+    console.log(`Throwing a ${code} error`)
+    res.setHeader('Content-Type', "text/plain");
+    res.writeHead(code);
+    res.end("ERROR: Please provide two numbers in the URL")
+}
+
+const listener = (req, res) => {
+    const numbers = req.url.toLowerCase().slice(1).split("/");
+    if (numbers.length !== 2) {
+        throwError(res, 501);
+        return;
+    }
+    for (let i = 0; i < numbers.length; i++){
+        const el = numbers[i];
+        if (/^[0-9]*$/.test(el)) {
+            numbers[i] = parseInt(el);
+        } else {
+            throwError(res, 501);
+            return;
+        }
+    }
+    console.log(numbers);
+    const result = numbers[0] * numbers[1];
+    res.setHeader('Content-Type', "text/plain");
+    res.writeHead(200);
+    res.end(result.toString());
+}
+
+const server = http.createServer(listener)
+
+server.listen(port, hostname, () => {
+    console.info(`Server is running at http://${hostname}:${port}`)
+})
