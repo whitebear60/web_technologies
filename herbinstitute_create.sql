7c983971687d82921ebaf9b4b213112f40d0c001
Mykhailo Yaremenko <mykhailo.yaremenko@outlook.com>
2024-05-01 14:35:03 +0300

    Add the mock data to the DB initialization script

--- herbinstitute_create.sql
+++ herbinstitute_create.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS `herb_institute`;
 CREATE DATABASE IF NOT EXISTS `herb_institute`;
 USE `herb_institute`;
 
@@ -162,3 +163,195 @@ CREATE TABLE IF NOT EXISTS `PURCHASE`
 -- Add another foreign key
 ALTER TABLE `PURCHASE` ADD CONSTRAINT `batch_id_fk` FOREIGN KEY (`batch_id`)
     REFERENCES `BATCH`(`batch_id`);
+    
+INSERT INTO `SORT` (`name`, `year`, `adaptation`, `frost`, `description`, `approved`, `period`) VALUES 
+
+('Tulipa agenensis', 2024, 1, 0, 'The flowers are brick red or deep red with black and yellow markings toward the center with a green stem. The petals are oval, tapered with curled tips and it has green and lanceolate foliage.', 1, 1), 
+
+('Malus Abbomdanza', 2024, 1, TRUE, 'Width 70–75 mm, height 65 mm. Cold Storage 3C 150 days.', 1, 2), 
+
+('Malus domestica Adams Pearmain', 2024, 0, 0, 'A dessert apple. First introduced under the name "Norfolk Pippin". Width 64 mm (2.5 in), height 63 mm (2.5 in). Cells obovate, abaxile. Flesh pale yellow, crisp,fine-textured, firm, juicy, aromatic.', 1, 2), 
+
+('Malus Admiral', 2024, 1, 0, 'New very popular variety. Flesh firm, crisp, juicy with very good taste. Robust, healthy, scab resistant variety with excellent keeping qualities and taste', 1, 2); 
+
+ 
+
+-- Add new values to the sort table without the description 
+
+INSERT INTO `SORT` (`name`, `year`, `adaptation`, `frost`, `approved`, `period`) VALUES 
+
+('Malus Granny Smith', 2024, 0, TRUE, 0, 3), 
+
+('Cucumis melo', 2024, 1, false, 1, 2), 
+
+('Malus pumila Antonovka', 1800, true, false, true, 3), 
+
+('Malus Mere de Menage', 1800, true, true, true, 3), 
+
+('Antonovka', 1800, true, true, true, 3), 
+
+('Weißer Wintertaffetapfel', 1797, true, true, true, 3); 
+
+ 
+
+# DROP TRIGGER new_sort_date; 
+
+ 
+
+INSERT INTO herb_institute.NEW_SORT (new_sort_id, date, comment) VALUES 
+
+(1, CURDATE(), 'Досліджено поведінку у сухих умовах'), 
+
+(2, CURDATE(), 'Досліджено поведінку у вологих умовах'), 
+
+(3, CURDATE(), 'Потрібно дослідити поведінку у сухих умовах'), 
+
+(4, CURDATE(), 'Дослідження завершено'), 
+
+(5, CURDATE(), 'Направити на схвалення інспекцією'), 
+
+(6, '2016-12-20', 'Досліджено поведінку у сухих умовах'); 
+
+ 
+
+-- Add the descriptions with UPDATE statements 
+
+UPDATE `SORT` 
+
+SET `description`='Apples are large, yellow-green and bracingly tart to eat out of hand, but superb for cooking, as they keep their shape.' 
+
+WHERE `name`='Antonovka'; 
+
+ 
+
+UPDATE `SORT` 
+
+SET `description`='Large flat-round yellow apple with crimson flush (50–100%). Width 77–128 mm (3.0–5.0 in), height 67–95 mm (2.6–3.7 in). Stalk very stout (4–5 mm) and short (10–15 mm). Cells roundish, obovate, abaxile' 
+
+WHERE `name`='Malus Mere de Menage'; 
+
+ 
+
+ 
+
+INSERT INTO `SELLER` (`seller_name`) VALUES 
+
+                                         ('Ivanov Ivan'), 
+
+                                         ('Petrov Petro'), 
+
+                                         ('Sydorov Sydir'), 
+
+                                         ('Yaremenko Mykhailo'), 
+
+                                         ('Fedorchenko Oleksandra'), 
+
+                                         ('Prots Maksym'), 
+
+                                         ('Protsenko Vitalii'), 
+
+                                         ('Meleshko Ivan'), 
+
+                                         ('Kolomyiets Ivanna'), 
+
+                                         ('Shevchenko Ganna'); 
+
+ 
+
+UPDATE `SELLER` SET `seller_name`='Shevchenko Hanna' WHERE `seller_name`='Shevchenko Ganna'; 
+
+ 
+
+INSERT INTO `PACKING` (`packing_name`, `amount`) VALUES ('Small', 10), 
+
+    	('Medium', 25), 
+
+                                                        ('Large', 50), 
+
+    ('Huge', 100); 
+
+ 
+
+INSERT INTO `BATCH` (`end`, `packing_id`, `packing_date`, `weight`, `price`, `sort_id`, `batch_size`) 
+
+     VALUES (DATE_ADD(CURDATE(), INTERVAL 90 DAY), 1, CURDATE(), 40, 200, 1, 10), 
+
+(DATE_ADD(CURDATE(), INTERVAL 90 DAY), 2, CURDATE(), 40, 200, 2, 30), 
+
+(DATE_ADD(CURDATE(), INTERVAL 180 DAY), 2, DATE_ADD(CURDATE(), INTERVAL 30 DAY), 50, 100, 2, 10); 
+
+ 
+
+INSERT INTO CLIENT (company, address, phone) VALUES 
+
+('ТОВ "Золоте Поле"', 'вул. Сонячна, с. Нове Життя, Вінницька область, Україна', '+380 432 123 456'), 
+
+('ТОВ "Урожайна Долина"', 'вул. Шевченка, 24, м. Рівне, Рівненська область, Україна', '+380 362 654 321'), 
+
+('ВАТ "Полісся-Агро"', 'м. Луцьк, вул. Центральна, 7, Волинська область, Україна', '+380 332 987 654'), 
+
+('ПАТ "Дніпровські Зорі"', 'вул. Шевченка, 56, м. Кременчук, Полтавська область, Україна', '+380 536 345 678'), 
+
+('ПрАТ "Поляничка"', 'с. Зелене Поле, вул. Садова, 10, Хмельницька область, Україна', '+380 382 876 543'), 
+
+('ТОВ "Зелений Вітер"', 'вул. Гагаріна, 2, м. Житомир, Житомирська область, Україна', '+380 412 234 567'), 
+
+('КП "Одеса-Агро"', 'м. Одеса, вул. Лісова, 5, Одеська область, Україна', '+380 482 765 432'), 
+
+('ТОВ "Барвисте Поле"', 'с. Веселе, вул. Поляна, 3, Донецька область, Україна', '+380 622 876 543'), 
+
+('АТ "Агрофірма "Соняшникове Поле"', 'вул. Шевченка, 18, м. Херсон, Херсонська область, Україна', '+380 552 987 654'), 
+
+('ТДВ "Зоряний Сад"', 'с. Полісся, вул. Садова, 9, Чернігівська область, Україна', '+380 462 345 678'); 
+
+ 
+
+UPDATE `CLIENT` SET `phone`='+380 462 345 444' WHERE `company`='ТДВ "Зоряний Сад"'; 
+
+ 
+
+INSERT INTO `BATCH` 
+
+(`end`, `packing_id`, `packing_date`, `weight`, `price`, `sort_id`, `batch_size`) 
+
+VALUES 
+
+    (DATE_ADD(CURDATE(), INTERVAL 1 YEAR), 1, DATE_SUB(CURDATE(), INTERVAL 3 WEEK), 60, 3000, 5, 100), 
+
+    (DATE_ADD(CURDATE(), INTERVAL 2 YEAR), 2, DATE_SUB(CURDATE(), INTERVAL 1 DAY), 180, 3600, 5, 80), 
+
+    (DATE_ADD(CURDATE(), INTERVAL 1 YEAR), 2, DATE_SUB(CURDATE(), INTERVAL 2 WEEK), 200, 200, 1, 100), 
+
+    (DATE_ADD(CURDATE(), INTERVAL 1 YEAR), 3, DATE_SUB(CURDATE(), INTERVAL 1 WEEK), 180, 1000, 5, 100), 
+
+    (DATE_ADD(CURDATE(), INTERVAL 1 YEAR), 3, DATE_SUB(CURDATE(), INTERVAL 1 MONTH), 250, 3600, 2, 120), 
+
+    (DATE_ADD(CURDATE(), INTERVAL 4 YEAR), 4, DATE_SUB(CURDATE(), INTERVAL 1 WEEK), 100, 450, 6, 50), 
+
+    (DATE_ADD(CURDATE(), INTERVAL 1 YEAR), 4, DATE_SUB(CURDATE(), INTERVAL 1 DAY), 100, 4500, 3, 10), 
+
+    (DATE_ADD(CURDATE(), INTERVAL 2 YEAR), 4, CURDATE(), 100, 2500, 10, 10); 
+
+ 
+
+INSERT INTO `PURCHASE` (`client_id`, `batch_id`, `buy_date`, `seller_id`) VALUES 
+
+(1, 1, CURDATE(), 7), 
+
+(6, 2, CURDATE(), 7), 
+
+(2, 1, CURDATE(), 3), 
+
+(8, 7, CURDATE(), 6), 
+
+(7, 5, CURDATE(), 8), 
+
+(3, 10, CURDATE(), 2), 
+
+(7, 3, CURDATE(), 1), 
+
+(2, 6, CURDATE(), 4), 
+
+(1, 1, CURDATE(), 1), 
+
+(3, 5, CURDATE(), 3); 
